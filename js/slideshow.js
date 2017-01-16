0d6a7c0c909d31ffbc3c373d0415c7b60144c353
dianyingx007@163.com <dianyingx007@163.com>
2017-01-16 17:13:29 +0800

    代码优化，并添加下排的点击按钮

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,24 +1,39 @@
 $(document).ready(function(){
 	var container=$('#container');
 	var list=$('#list');
-	var button_left=$('#button_left');
-	var button_right=$('#button_right');
+	var buttons=$('#buttons span');
+	var arrow_left=$('#arrow_left');
+	var arrow_right=$('#arrow_right');
 	var pic_width=$('#list img').innerWidth();
 	var ani_flag=0;/*判断是否正在运行动画*/
+	var index=1;/*存放当前的图片序号*/
 	
 	function move_left(){
 		if(ani_flag===0){
-			ani_flag=1;
+			changeButton();
+			if(index===1){
+				index=4;
+			}else {
+				index-=1;
+			}
+			changeButton();
 			animation(pic_width);
 		}
 	}
 	function move_right(){
 		if(ani_flag===0){
-			ani_flag=1;
+			changeButton();
+			if(index===4){
+				index=1;
+			}else {
+				index+=1;
+			}
+			changeButton();
 			animation(-pic_width);
 		}
 	}
 	function animation(move_step){
+		ani_flag=1;
 		var step=10;/*分多少步演示动画*/
 		var time=500;/*动画一共多少时间*/
 		
@@ -38,19 +53,35 @@ $(document).ready(function(){
 				ani_flag=0;
 			}
 		},time/step);
-		
 	}
-	button_left.on('click',move_left);
-	button_right.on('click',move_right);
+	arrow_left.on('click',move_left);
+	arrow_right.on('click',move_right);
+	
+	/*下排的button按钮响应改变*/
+	function changeButton(){
+		$(buttons[index-1]).toggleClass("button_on");
+		/*jQuery中选择出来的数组，在用[i]选择其中之一时，其结果为DOM对象，不是jQuery对象*/
+	}
+	
+	/*添加button按钮点击的事件，跳转到相应的图片*/
+	for(var i=0;i<buttons.length;i++){
+		$(buttons[i]).on('click',function(){
+			if(!$(this).hasClass("button_on")){
+				var myindex=parseInt($(this).attr('index'));/*获取的属性是字符串类型，需要转换*/
+				changeButton();
+				animation((index-myindex)*pic_width);
+				index=myindex;
+				changeButton();
+			}
+		});
+	}
 	
 	/*以下是自动播放功能*/
 	var timer=setInterval(move_right,3000);
 	container.on('mouseenter',function(){
-		console.log(1);
 		clearInterval(timer);
-	});
+	});/*鼠标在图片上面时，停止播放*/
 	container.on('mouseleave',function(){
-		console.log(0);
 		timer=setInterval(move_right,3000);
 	});
 });
