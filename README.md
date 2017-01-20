5e4ad033cdca0be1be6e3ec1c871395514f25366
dianyingx007@163.com <dianyingx007@163.com>
2017-01-20 10:40:59 +0800

    更新README.md并增加参数

--- README.md
+++ README.md
@@ -2,8 +2,98 @@
 
 ##层叠式焦点图轮播
 
-文件：index.html slideshow3d.scss slideshow3d.js
+文件：index2.html slideshow3d.scss slideshow3d.js
 
-##普通焦点图轮播
+##水平焦点图轮播
 
-文件：index.html slideshow.css slideshow.js
+文件：index1.html slideshow.css slideshow.js
+
+###v1.0
+
+####缺陷：
+
+轮播图的布局宽度未能实现自适应
+
+**补救措施**需要使用js读取宽度后，设置示例中的container、list、list_img设置宽度，以及list的初始left值，并添加resize事件
+
+####注意事项：
+* 依赖jQuery
+* 对象参数的传入必须为jQuery对象
+* html布局时，注意首尾增加一个图片层，实现无限循环
+* 参数是否可以为空，在下表
+
+####可用参数：
+
+<table>
+	<tr>
+		<th>参数</th>
+		<th>默认值</th>
+		<th>解释</th>
+	</tr>
+	<tr>
+		<td>container</td>
+		<td>null</td>
+		<td>整个控件的层，当鼠标移动到上面时，会停止自动播放</td>
+	</tr>
+	<tr>
+		<td>*list</td>
+		<td>null</td>
+		<td>整个图片层(各个图片层的上一层)</td>
+	</tr>
+	<tr>
+		<td>*buttons</td>
+		<td>null</td>
+		<td>图片对应的每个按钮</td>
+	</tr>
+	<tr>
+		<td>*pre</td>
+		<td>null</td>
+		<td>前一个图片按钮</td>
+	</tr>
+	<tr>
+		<td>*next</td>
+		<td>null</td>
+		<td>后一个图片按钮</td>
+	</tr>
+	<tr>
+		<td>*pic_width</td>
+		<td>0</td>
+		<td>图片的宽度</td>
+	</tr>
+	<tr>
+		<td>num</td>
+		<td>2</td>
+		<td>图片数量</td>
+	</tr>
+	<tr>
+		<td>time</td>
+		<td>3000</td>
+		<td>自动播放的间隔时间（以毫秒为单位）</td>
+	</tr>
+	<tr>
+		<td>speed</td>
+		<td>500</td>
+		<td>图片切换速度（同上）</td>
+	</tr>
+	<tr>
+		<td>auto</td>
+		<td>1</td>
+		<td>是否自动播放，不为0则表示自动播放</td>
+	</tr>
+</table>
+
+**其中加*为必须传入参数**
+
+####示例如下：
+
+		slideshow({
+			container:$('#container'),
+			list:$('#list'),
+			buttons:$('#buttons span'),
+			pre:$('#arrow_left'),
+			next:$('#arrow_right'),
+			pic_width:1000,
+			num:4
+		});
+		
+可观察index1.html示例
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,90 +1,91 @@
-$(document).ready(function(){
-	/*封装的函数*/
-	function slideshow(option){
-		var defaults = {
-			container:$('#container'),/*整个控件的层，当鼠标移动到上面时，会停止自动播放*/
-			list:$('#list'),/*整个图片层*/
-			buttons:$('#buttons span'),/*图片对应的每个按钮*/
-			pre:$('#arrow_left'),/*前一个图片*/
-			next:$('#arrow_right'),/*后一个图片*/
-			pic_width:1000,/*图片的宽度*/
-			num:2,/*图片数量*/
-			time:3000,/*自动播放的间隔时间*/
-			speed:500/*图片切换速度*/
-		};
-		var params = $.extend({},defaults,option || {});
-		var ani_flag=0;/*判断是否正在运行动画*/
-		var index=1;/*存放当前的图片序号*/
+/*封装的函数*/
+function slideshow(option){
+	var defaults = {
+		container:null,/*整个控件的层，当鼠标移动到上面时，会停止自动播放*/
+		list:null,/*整个图片层*/
+		buttons:null,/*图片对应的每个按钮*/
+		pre:null,/*前一个图片按钮*/
+		next:null,/*后一个图片按钮*/
+		pic_width:0,/*图片的宽度*/
+		num:2,/*图片数量*/
+		time:3000,/*自动播放的间隔时间*/
+		speed:500,/*图片切换速度*/
+		auto:1/*是否自动播放*/
+	};
+	var params = $.extend({},defaults,option || {});
+	var ani_flag=0;/*判断是否正在运行动画*/
+	var index=1;/*存放当前的图片序号*/
 
-		function move_left(){
-			if(ani_flag===0){
-				changeButton();
-				if(index===1){
-					index=params['num'];
-				}else {
-					index-=1;
-				}
-				changeButton();
-				animation(params['pic_width']);
+	function move_left(){
+		if(ani_flag===0){
+			changeButton();
+			if(index===1){
+				index=params['num'];
+			}else {
+				index-=1;
 			}
+			changeButton();
+			animation(params['pic_width']);
 		}
-		function move_right(){
-			if(ani_flag===0){
-				changeButton();
-				if(index===params['num']){
-					index=1;
-				}else {
-					index+=1;
-				}
-				changeButton();
-				animation(-params['pic_width']);
+	}
+	function move_right(){
+		if(ani_flag===0){
+			changeButton();
+			if(index===params['num']){
+				index=1;
+			}else {
+				index+=1;
 			}
+			changeButton();
+			animation(-params['pic_width']);
 		}
-		function animation(move_step){
-			ani_flag=1;
-			var step=10;/*分多少步演示动画*/
-			var time=params['speed'];/*动画一共多少时间*/
-			
-			var onestep=move_step/step;
-			var animationid=setInterval(function(){
-				params['list'].css('left',parseInt(params['list'].css('left'))+onestep+'px');
-				step--;
-				if(step===0){
-					clearInterval(animationid);
+	}
+	function animation(move_step){
+		ani_flag=1;
+		var step=10;/*分多少步演示动画*/
+		var time=params['speed'];/*动画一共多少时间*/
+		
+		var onestep=move_step/step;
+		var animationid=setInterval(function(){
+			params['list'].css('left',parseInt(params['list'].css('left'))+onestep+'px');
+			step--;
+			if(step===0){
+				clearInterval(animationid);
 
-					if(params['list'].css('left')===-(params['num']+1)*params['pic_width']+'px'){
-						params['list'].css('left',-params['pic_width']+'px');
-					}else if(params['list'].css('left')==='0px'){
-						params['list'].css('left',-params['num']*params['pic_width']+'px');
-					}/*首尾页循环*/
-					
-					ani_flag=0;
-				}
-			},time/step);
-		}
-		params['pre'].on('click',move_left);
-		params['next'].on('click',move_right);
-		
-		/*下排的button按钮响应改变*/
-		function changeButton(){
-			$(params['buttons'][index-1]).toggleClass("button_on");
-			/*jQuery中选择出来的数组，在用[i]选择其中之一时，其结果为DOM对象，不是jQuery对象*/
-		}
-		
-		/*添加button按钮点击的事件，跳转到相应的图片*/
-		for(var i=0;i<params['buttons'].length;i++){
-			$(params['buttons'][i]).on('click',function(){
-				if(!$(this).hasClass("button_on")){
-					var myindex=parseInt($(this).attr('index'));/*获取的属性是字符串类型，需要转换*/
-					changeButton();
-					animation((index-myindex)*params['pic_width']);
-					index=myindex;
-					changeButton();
-				}
-			});
-		}
-		
-		/*以下是自动播放功能*/
+				if(params['list'].css('left')===-(params['num']+1)*params['pic_width']+'px'){
+					params['list'].css('left',-params['pic_width']+'px');
+				}else if(params['list'].css('left')==='0px'){
+					params['list'].css('left',-params['num']*params['pic_width']+'px');
+				}/*首尾页循环*/
+				
+				ani_flag=0;
+			}
+		},time/step);
+	}
+	params['pre'].on('click',move_left);
+	params['next'].on('click',move_right);
+	
+	/*下排的button按钮响应改变*/
+	function changeButton(){
+		$(params['buttons'][index-1]).toggleClass("button_on");
+		/*jQuery中选择出来的数组，在用[i]选择其中之一时，其结果为DOM对象，不是jQuery对象*/
+	}
+	
+	/*添加button按钮点击的事件，跳转到相应的图片*/
+	for(var i=0;i<params['buttons'].length;i++){
+		$(params['buttons'][i]).on('click',function(){
+			if(!$(this).hasClass("button_on")){
+				var myindex=parseInt($(this).attr('index'));/*获取的属性是字符串类型，需要转换*/
+				changeButton();
+				animation((index-myindex)*params['pic_width']);
+				index=myindex;
+				changeButton();
+			}
+		});
+	}
+	
+	/*以下是自动播放功能*/
+	if(params['auto']){
 		var timer=setInterval(move_right,params['time']);
 		params['container'].on('mouseenter',function(){
 			clearInterval(timer);
@@ -93,17 +94,4 @@ $(document).ready(function(){
 			timer=setInterval(move_right,params['time']);
 		});
 	}
-
-
-
-	/*调用封装的函数*/
-	slideshow({
-		container:$('#container'),
-		list:$('#list'),
-		buttons:$('#buttons span'),
-		pre:$('#arrow_left'),
-		next:$('#arrow_right'),
-		pic_width:1000,
-		num:4
-	});
-});
+}
